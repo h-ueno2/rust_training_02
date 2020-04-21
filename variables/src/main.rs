fc99ae51b6833503c966db5e4fbec552795a3f08
h-ueno2 <menbou1212@gmail.com>
2020-04-21 21:58:49 +0900

    3.3 関数の動作法

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -29,4 +29,34 @@ fn main() {
     let (x, y, z) = tup;
     println!("The value of y is: {}", y);
     println!("the tup.1 is: {}", tup.1);
+
+    another_function(5);
+
+    let x = 5;
+    let y = {
+        let x = 3;
+        x + 1
+    };
+
+    println!("The value of y is: {}", y); // 4
+    println!("The value of x is: {}", x); // 5
+
+    let x = five();
+    println!("The value of x is: {}", x);
+
+    let x = plus_one(5);
+    println!("The value of x is: {}", x);
+}
+
+fn another_function(x: i32) {
+    println!("Another function.");
+    println!("The value of x is: {}", x)
+}
+
+fn five() -> i32 {
+    5
+}
+
+fn plus_one(x: i32) -> i32 {
+    x + 1
 }
