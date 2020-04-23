a5391eb751c929b524c2f72d3dec4f7b44f7e308
h-ueno2 <menbou1212@gmail.com>
2020-04-23 20:19:21 +0900

    4.2 参照と借用

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -23,6 +23,40 @@ fn main() {
     let s3 = takes_and_gives_back(s2);
     // println!("{}", s2); // これはコンパイルエラーになる。所有権が関数に移動しているため。
     println!("{}", s3);
+
+    // `&`を付けることで所有権を関数に渡さない。
+    let s1 = String::from("hello");
+    let len = calculate_length(&s1);
+    println!("The length of `{}` is {}.", s1, len);
+    
+    let mut s = String::from("hello");
+    change(&mut s);
+    println!("{}", s);
+
+    // 以下はドキュメントだとエラーになるって書いてあるけど
+    // コンパイルできてしまう。
+    let mut st = String::from("hello");
+    let r1 = &st;
+    let r2 = &st;
+    let r3 = &mut st;
+    r3.push_str("a");
+
+    let reference_to_nothing = dangle();
+}
+
+// fn dangle() -> &String { // Stringへの参照を返却する
+fn dangle() -> String {
+    let s = String::from("hello");
+    // &s // String s への参照を返そうとするが、sはスコープを抜けたら終了するためエラーとなる。
+    s // sを直接返すのであれば問題ない。
+}
+
+fn calculate_length(s: &String) -> usize {
+    s.len()
+}
+
+fn change(some_string: &mut String) {
+    some_string.push_str(", world");
 }
 
 fn takes_ownership(some_string: String) {
