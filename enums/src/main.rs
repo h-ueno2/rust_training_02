6741062efce0f3c1871380c5b2994979d0817715
h-ueno2 <menbou1212@gmail.com>
2020-04-25 21:45:47 +0900

    6.2 match制御フロー演算子

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -2,6 +2,10 @@ fn main() {
     let home = IpAddr::V4(String::from("127.0.0.1"));
     
     let loopback = IpAddr::V6(String::from("::1"));
+
+    let five = Some(5);
+    let six = plus_one(five);
+    println!("{:#?}",six);
 }
 
 enum IpAddrKind {
@@ -12,4 +16,35 @@ enum IpAddrKind {
 enum IpAddr {
     V4(String),
     V6(String),
+}
+
+// enum Option<T> {
+//     Some(T),
+//     None,
+// }
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
+    }
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter,
+}
+
+fn value_in_cents(coin: Coin) -> u32 {
+    match coin {
+        Coin::Penny => {
+            println!("Lucky penny");
+            1
+        },
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter => 25,
+    }
 }
