6f06205c7aa43d33c9d56155cd5aeecf312f415f
h-ueno2 <menbou1212@gmail.com>
2020-04-25 22:00:37 +0900

    6.3 if let

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -6,6 +6,16 @@ fn main() {
     let five = Some(5);
     let six = plus_one(five);
     println!("{:#?}",six);
+
+    let some_u8_value = Some(0u8);
+    // match some_u8_value {
+    //     Some(3) => println!("three"),
+    //     _ => (),
+    // };
+
+    if let Some(3) = some_u8_value {
+        println!("three");
+    }
 }
 
 enum IpAddrKind {
