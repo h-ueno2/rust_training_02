73daf621f389c95f32b1c5b5a74564f4acc4f2cc
h-ueno2 <menbou1212@gmail.com>
2020-04-26 10:37:45 +0900

    7.3 異なるモジュールの名前を参照する

--- communicator/src/main.rs
+++ communicator/src/main.rs
@@ -1,5 +1,29 @@
 extern crate communicator;
 
+pub mod a {
+    pub mod series {
+        pub mod of {
+            pub fn nested_modules() {}
+        }
+    }
+}
+
+enum TrafficLight {
+    Red,
+    Yellow,
+    Greeen,
+}
+
+use a::series::of::nested_modules;
+// use TrafficLight::{Red, Yellow};
+use TrafficLight::*;
+
 fn main() {
-    communicator::client::connect();
+    a::series::of::nested_modules();
+    nested_modules(); // useキーワードで指定しているため、上と同義
+
+    let red = Red;
+    let yellow = Yellow;
+    // let green = TrafficLight::Greeen; // Greenは指定していないためフル
+    let green = Greeen;
 }
