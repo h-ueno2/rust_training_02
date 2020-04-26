21c5b508f8736b748b3220fcc8c8a14b7557addd
h-ueno2 <menbou1212@gmail.com>
2020-04-26 11:48:43 +0900

    8.3 ハッシュマップ

--- collections_sample/src/main.rs
+++ collections_sample/src/main.rs
@@ -1,8 +1,58 @@
+use std::collections::HashMap;
+
 fn main() {
     vector_example();
 
     string_example();
+
+    hashmap_example();
 }
+
+fn hashmap_example() {
+    let mut scores = HashMap::new();
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+
+    let teams = vec![String::from("Blue"), String::from("Yellow")];
+    let initial_scores = vec![10, 50];
+    let scores: HashMap<_, _> = teams.iter().zip(initial_scores.iter()).collect();
+
+    // HashMapからの取得
+    let team_name = String::from("Blue");
+    let score = scores.get(&team_name);
+    println!("score :{:?}", score);
+
+    for (key, value) in &scores {
+        println!("{}: {}", key, value);
+    }
+
+    // 所有権について
+    let field_name = String::from("Favorite color");
+    let field_value = String::from("Blue");
+    let mut map = HashMap::new();
+    map.insert(field_name, field_value);
+    // println!("{}", field_value); // 所有権が移動しているためエラーとなる
+
+
+    // キーに値がなかった時のみ挿入
+    let mut scores = HashMap::new();
+    scores.insert(String::from("Blue"), 10);
+
+    scores.entry(String::from("Yellow")).or_insert(50);
+    scores.entry(String::from("Blue")).or_insert(50);
+
+    println!("{:?}", scores);
+
+    // 値に基づいて更新
+    let text = "hello world wonderful world";
+    let mut map = HashMap::new();
+    for word in text.split_whitespace() {
+        let count = map.entry(word).or_insert(0);
+        *count += 1;
+    }
+    println!("{:?}", map);
+}
+
 
 fn vector_example() {
     let v: Vec<i32> = Vec::new();
