b4e734d54ac0955ba520abba2458637f663d7080
h-ueno2 <menbou1212@gmail.com>
2020-04-29 14:18:09 +0900

    10.2 トレイト

--- generics/src/main.rs
+++ generics/src/main.rs
@@ -1,15 +1,28 @@
-// fn largest<T>(list: &[T]) -> T {
-//     let mut largest = list[0];
-//     for &item in list.iter() {
-//         // `>`がTに適用できないためエラーとなる
-//         if item > largest {
-//             largest = item;
-//         }
-//     }
-//     largest
-// }
+fn largest<T: PartialOrd + Copy>(list: &[T]) -> T {
+    let mut largest = list[0];
+    for &item in list.iter() {
+        // `>`がTに適用できないためエラーとなる
+        if item > largest {
+            largest = item;
+        }
+    }
+    largest
+}
+extern crate generics;
+
+use generics::summary::*;
 
 fn main() {
+    let number_list = vec![34, 50, 25, 100, 65];
+
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
+
+    let char_list = vec!['y', 'm', 'a', 'q'];
+
+    let result = largest(&char_list);
+    println!("The largest char is {}", result);
+
     // x, y 共にTのため、同じ型である必要がある。
     let integer = Point { x: 5, y: 10 };
     let float = Point { x: 1.0, y: 4.0 };
@@ -19,6 +32,14 @@ fn main() {
     let p2 = Point2 { x: "Hello", y: 'c' };
     let p3 = p1.mixup(p2);
     println!("p3.x = {}, p3.y = {}", p3.x, p3.y);
+
+    let tweet = Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from("of course, as you probably already know, people"),
+        reply: false,
+        retweet: false,
+    };
+    println!("1 new tweet: {}", tweet.summarize());
 }
 
 struct Point<T> {
