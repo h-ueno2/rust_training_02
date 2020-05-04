f0fc78e6c039e66f19d6980ef7a845dc064df340
h-ueno2 <menbou1212@gmail.com>
2020-05-04 14:10:08 +0900

    13.2 イテレータ

--- closure_sample/src/main.rs
+++ closure_sample/src/main.rs
@@ -1,12 +1,6 @@
 use std::thread;
 use std::time::Duration;
 
-// fn simulated_expensive_calculation(intensity: u32) -> u32 {
-//     println!("calculation slowly...");
-//     thread::sleep(Duration::from_secs(2));
-//     intensity
-// }
-
 fn main() {
     let simulated_user_specified_value = 10;
     let simulated_random_number = 7;
